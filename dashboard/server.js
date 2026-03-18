d23da1529f5efe3707f2b2861f2b10f5f70006c4
AlbinoClaw <bilytech.com@gmail.com>
2026-03-18 05:17:37 +0100

    Add system status API

--- dashboard/server.js
+++ dashboard/server.js
@@ -10,7 +10,28 @@ app.use(express.static(path.join(__dirname, "frontend")))
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "index.html"))
 })
+app.get("/system", (req, res) => {
 
+  const system = {
+    agents: [
+      "product_manager",
+      "planner",
+      "supervisor",
+      "memory",
+      "ui_designer",
+      "developer"
+    ],
+
+    status: "running",
+
+    tasks: 0,
+
+    logs: "system initialized"
+  }
+
+  res.json(system)
+
+})
 app.listen(PORT, () => {
   console.log("Dashboard running on http://localhost:" + PORT)
 })
