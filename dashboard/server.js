8d6e44dc64b1a1a2a770a6e5f88f7565e5c8f2e4
AlbinoClaw <bilytech.com@gmail.com>
2026-03-18 05:15:07 +0100

    Connect dashboard server to frontend

--- dashboard/server.js
+++ dashboard/server.js
@@ -1,12 +1,16 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
 const PORT = 3000
 
+// serve frontend files
+app.use(express.static(path.join(__dirname, "frontend")))
+
 app.get("/", (req, res) => {
-  res.send("OpenClaw AI Company Dashboard Running")
+  res.sendFile(path.join(__dirname, "frontend", "index.html"))
 })
 
 app.listen(PORT, () => {
-  console.log("Dashboard running on port " + PORT)
+  console.log("Dashboard running on http://localhost:" + PORT)
 })
